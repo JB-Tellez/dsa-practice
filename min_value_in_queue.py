8988092b54c3af9127def7d4c2ed4a21b23bb8c5
JB-Tellez <jb.tellez@gmail.com>
2022-02-08 21:56:59 -0800

    adds min-value-in-queue and sum-linked-list

---
+++ min_value_in_queue.py
@@ -0,0 +1,8 @@
+def min_value(q):
+    lowest = q.dequeue()
+    while(q):
+        candidate = q.dequeue()
+        if candidate < lowest:
+            lowest = candidate
+
+    return lowest
---
+++ test_sum_linked_list.py
@@ -0,0 +1,8 @@
+from codefellows.dsa.linked_list import LinkedList
+from sum_linked_list import sum_linked_list
+
+def test_sum_linked_list():
+    nums = LinkedList(values=[1,2,3,4,5])
+    actual = sum_linked_list(nums.head)
+    expected = 15
+    assert actual == expected
